097167edb1c9c54c7c7e1ae82eed3f4fc90ca12b
qmtazmir <96815202+qmtazmir@users.noreply.github.com>
2022-08-02 10:09:23 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXEzpyqRgsodCHv-9ls8UCeCma4bGQIuY",
+  authDomain: "fir-router-integration-5e576.firebaseapp.com",
+  projectId: "fir-router-integration-5e576",
+  storageBucket: "fir-router-integration-5e576.appspot.com",
+  messagingSenderId: "381882389880",
+  appId: "1:381882389880:web:d4195e7fa63c961d8d248d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
